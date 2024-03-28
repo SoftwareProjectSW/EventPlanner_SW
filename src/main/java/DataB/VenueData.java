566aed78662330828f5f7b42881e22308c85df4a
RaghadThawabi <160324425+RaghadThawabi@users.noreply.github.com>
2024-03-28 22:01:58 +0200

    Update VenueData.java

--- src/main/java/DataB/VenueData.java
+++ src/main/java/DataB/VenueData.java
@@ -65,8 +65,9 @@ public class VenueData {
     public void readVenueDataFromFile() {
 
 
+        BufferedReader reader = null;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(VenueFile));
+            reader = new BufferedReader(new FileReader(VenueFile));
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
@@ -82,6 +83,14 @@ public class VenueData {
             reader.close();
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Error closing BufferedWriter: " + e.getMessage());
+                }
+            }
         }
 
     }
