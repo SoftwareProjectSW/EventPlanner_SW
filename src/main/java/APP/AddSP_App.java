26b9e73ce23a39b860953fb1ac821b57039cf80b
RaghadThawabi <160324425+RaghadThawabi@users.noreply.github.com>
2024-03-29 01:01:39 +0200

    Update AddSP_App.java

--- src/main/java/APP/AddSP_App.java
+++ src/main/java/APP/AddSP_App.java
@@ -1,10 +1,7 @@
 package APP;
 
-import DataB.AdminData;
 import DataB.ServiceProviderData;
-import io.cucumber.java.sl.In;
-import org.example.AdminClass;
-import org.example.ServiceProviderClass;
+
 import javax.swing.*;
 
 import java.io.BufferedWriter;
@@ -21,9 +18,7 @@ public class AddSP_App {
     private boolean isSP_Added ;
     private boolean isAlredyExists;
 
-    public boolean isAlredyExists() {
-        return isAlredyExists;
-    }
+  
 
     public void setAlredyExists(boolean alredyExists) {
         isAlredyExists = alredyExists;
@@ -37,28 +32,19 @@ public class AddSP_App {
         this.isSPLineValide = isValidLine(inputLine);
     }
 
-    public String getInputLine() {
-        return inputLine;
-    }
 
     public boolean getIsSPLineValide() {
         return isSPLineValide;
     }
 
-    public boolean isSP_Added() {
-        return isSP_Added;
-    }
 
     public void setSP_Added(boolean SP_Added) {
         isSP_Added = SP_Added;
     }
 
-    // Method to check if the input line matches the required format
     public  boolean isValidLine(String line) {
-        // Split the line by comma to extract fields
         String[] fields = line.split(",");
 
-        // Check if the line has exactly 4 fields
         if (fields.length < 4) {
             System.out.println("few fields, please enter complete information !");
             return false;
@@ -88,19 +74,16 @@ public class AddSP_App {
         ServiceProviderData sp = new ServiceProviderData();
 
         String filename = "DataForSP.txt";
-        // Check if the line is valid
         if (isValidLine(line)) {
-            // Check if the unique id is already present in the file
             try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
                 String fileLines;
                 Set<String> ids = new HashSet<>();
 
-                // Read each line and extract unique ids
                 while ((fileLines = reader.readLine()) != null) {
 
                     String[] fields = fileLines.split(",");
                     if (fields.length >= 2) {
-                        ids.add(fields[1]); // Add unique ids to the set
+                        ids.add(fields[1]); 
                     }
                 }
 
@@ -132,13 +115,7 @@ public class AddSP_App {
     }
 
 
-    public void showMessage(){
-        if (isSP_Added) JOptionPane.showMessageDialog(null,"added the service provider with info: " +
-                inputLine+ " successfully", "SUCCESS",JOptionPane.INFORMATION_MESSAGE
-        );
-        else JOptionPane.showMessageDialog(null,"the service provider with info: " +
-                inputLine+" Already Exists", "FAIL",JOptionPane.WARNING_MESSAGE);
-    }
+  
 
     public void showMessageToEnterFullDetails(){
         JOptionPane.showMessageDialog(null,"please provide all required information "
@@ -146,21 +123,6 @@ public class AddSP_App {
 
     }
 
-
-    public void enterLineUntillValid(){
-        Scanner scanner = new Scanner(System.in);
-
-        while (!isSPLineValide){
-
-            this.inputLine = scanner.nextLine();
-            this.isSPLineValide = isValidLine(inputLine);
-
-        }
-        addLineToFile(inputLine);
-
-
-    }
-
-
+    
 
 }
