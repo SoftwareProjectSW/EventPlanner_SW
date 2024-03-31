8ef97e99c9b44b7b8cb062998ff9629d6c3921bc
RaghadThawabi <160324425+RaghadThawabi@users.noreply.github.com>
2024-03-31 04:01:18 +0200

    Update SuperProviderClass.java

--- src/main/java/org/example/SuperProviderClass.java
+++ src/main/java/org/example/SuperProviderClass.java
@@ -20,45 +20,7 @@ public class SuperProviderClass {
         this.name = name;
     }
 
-    public void setFood(String food) {
-        this.food = food;
-    }
-
-    public void setSongs(String songs) {
-        this.songs = songs;
-    }
-
-    public void setDesign(String design) {
-        this.design = design;
-    }
-
-    public void setDecor(String decor) {
-        this.decor = decor;
-    }
-
-    public void setMusic(String music) {
-        this.music = music;
-    }
-
-    public String getFood() {
-        return food;
-    }
-
-    public String getSongs() {
-        return songs;
-    }
-
-    public String getDesign() {
-        return design;
-    }
-
-    public String getDecor() {
-        return decor;
-    }
-
-    public String getMusic() {
-        return music;
-    }
+   
     @Override
     public String toString() {
         return "(Name) :  " + name +"\n"+ " , ( Food):  " + food + "\n"+" , ( Song) : " + songs +"\n"+ " , ( Design ):  " + design +"\n"+ " , ( Decor) : " + decor +"\n"+ " , ( Music) : " + music;
